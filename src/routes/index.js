57be8dd33289f9765ddc9512f5f9d36cb12ae4a9
Gerald <geraldjoly191@gmail.com>
2024-09-11 20:52:21 -0300

    Adding the album and artist MVC first part

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const routerSong = require('./song.router');
 const routerGenre = require('./genre.router');
+const routerArtist = require('./artist.router');
+const routerAlbum = require('./album.router');
 const router = express.Router();
 
 // colocar las rutas aquí
 router.use('/songs',routerSong)
 router.use('/genres',routerGenre)
-
+router.use('/artists',routerArtist)
+router.use('/albums',routerAlbum)
 module.exports = router;
